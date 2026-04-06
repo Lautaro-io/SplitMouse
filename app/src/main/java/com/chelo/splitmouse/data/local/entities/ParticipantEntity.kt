e81a2b2b128b8b44c765182b7ab5c6d51fe32851
Lautaro Ildarraz <112969613+Lautaro-io@users.noreply.github.com>
2026-04-06 16:54:42 -0300

    mapper

--- app/src/main/java/com/chelo/splitmouse/data/local/entities/ParticipantEntity.kt
+++ app/src/main/java/com/chelo/splitmouse/data/local/entities/ParticipantEntity.kt
@@ -2,6 +2,7 @@ package com.chelo.splitmouse.data.local.entities
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import com.chelo.splitmouse.domain.model.Participant
 
 
 @Entity("participants")
@@ -9,4 +10,11 @@ data class ParticipantEntity(
     @PrimaryKey(autoGenerate = true) val id : Long = 0 ,
     val name : String,
     val eventId : Long
+)
+
+
+fun ParticipantEntity.toModel() = Participant(
+    id = id,
+    name = name,
+    eventId = eventId
 )
