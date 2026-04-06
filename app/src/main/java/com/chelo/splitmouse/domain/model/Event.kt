33dae2f286834b910de18ca2519c7db0a6082ade
Lautaro Ildarraz <112969613+lautaro-io@users.noreply.github.com>
2026-04-06 16:52:28 -0300

    Se agrego metodo para mapear la entidad al model

--- app/src/main/java/com/chelo/splitmouse/domain/model/Event.kt
+++ app/src/main/java/com/chelo/splitmouse/domain/model/Event.kt
@@ -1,5 +1,7 @@
 package com.chelo.splitmouse.domain.model
 
+import com.chelo.splitmouse.data.local.entities.EventEntity
+
 
 data class Event(
     val id: Long ,
@@ -7,4 +9,12 @@ data class Event(
     val date: String,
     val description: String,
     val totalAmount: Double,
+)
+
+fun Event.toEntity() = EventEntity(
+    id = id,
+    name = name,
+    date = date,
+    description = description,
+    totalAmount = totalAmount
 )
