a72202f56065e207e7897258d6053ac4090b588a
Lautaro Ildarraz <112969613+Lautaro-io@users.noreply.github.com>
2026-04-06 16:54:33 -0300

    Se agrego query para obtener los participantes x cada evento

--- app/src/main/java/com/chelo/splitmouse/data/local/daos/ParticipantDao.kt
+++ app/src/main/java/com/chelo/splitmouse/data/local/daos/ParticipantDao.kt
@@ -3,6 +3,7 @@ package com.chelo.splitmouse.data.local.daos
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
+import androidx.room.Query
 import com.chelo.splitmouse.data.local.entities.ParticipantEntity
 
 
@@ -12,6 +13,10 @@ interface ParticipantDao {
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertParticipant(participant: ParticipantEntity)
+
+    @Query("SELECT * FROM participants where eventId = :eventId")
+    suspend fun getParticipantsByEvent(eventId : Long): List<ParticipantEntity>
+
 
 
 
