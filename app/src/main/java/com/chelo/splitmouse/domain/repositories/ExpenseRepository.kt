3df568197c1da6138dceca5e24cb2f1036434246
Lautaro Ildarraz <112969613+Lautaro-io@users.noreply.github.com>
2026-04-07 19:34:23 -0300

    Se cambio el nombre de la interface Expense por ExpenseRepository.kt para evitar conflictos

--- app/src/main/java/com/chelo/splitmouse/domain/repositories/ExpenseRepository.kt
+++ app/src/main/java/com/chelo/splitmouse/domain/repositories/ExpenseRepository.kt
@@ -1,8 +1,9 @@
 package com.chelo.splitmouse.domain.repositories
 
+import com.chelo.splitmouse.domain.model.Expense
 
 
-interface Expense {
+interface ExpenseRepository {
 
 
     suspend fun addExpense(expense: Expense)
