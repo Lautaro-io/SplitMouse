9801a9f069e175f5271072e0a2070a6066483e51
Lautaro Ildarraz <112969613+Lautaro-io@users.noreply.github.com>
2026-04-07 19:36:08 -0300

    Se agregaron componentes en la mainscreen para el test de eventos

--- app/src/main/java/com/chelo/splitmouse/screens/MainScreen.kt
+++ app/src/main/java/com/chelo/splitmouse/screens/MainScreen.kt
@@ -1,2 +1,143 @@
 package com.chelo.splitmouse.screens
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AddCircle
+import androidx.compose.material.icons.filled.SupervisedUserCircle
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavController
+import com.chelo.splitmouse.domain.model.Event
+import com.chelo.splitmouse.ui.theme.Pink40
+import com.chelo.splitmouse.ui.theme.Purple40
+import com.chelo.splitmouse.viewmodel.MainViewModel
+import kotlinx.coroutines.flow.asStateFlow
+import org.koin.androidx.compose.koinViewModel
+
+@Composable
+fun MainScreen(navController: NavController? = null, viewmodel: MainViewModel = koinViewModel() ) {
+
+    val state by viewmodel.uiState.collectAsState()
+    val events = state.events
+
+
+    Scaffold(
+        topBar = {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                Icon(imageVector = Icons.Default.SupervisedUserCircle, contentDescription = "")
+                Icon(imageVector = Icons.Default.SupervisedUserCircle, contentDescription = "")
+                Icon(imageVector = Icons.Default.SupervisedUserCircle, contentDescription = "")
+            }
+        },
+
+        ) { innerPadding ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Card(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 14.dp),
+                shape = RoundedCornerShape(16.dp),
+                elevation = CardDefaults.cardElevation(4.dp),
+                colors = CardDefaults.cardColors(
+                    containerColor = Pink40,
+                    contentColor = Color.Black
+                )
+            ) {
+                Column(
+                    modifier = Modifier.padding(32.dp),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Text(
+                        "¿Listos para otra salida?",
+                        fontSize = 38.sp,
+                        fontWeight = FontWeight.Bold,
+                        textAlign = TextAlign.Start
+                    )
+                    Text(
+                        "Organiza gastos, divide la cuenta y disfruta el momento",
+                        modifier = Modifier.padding(vertical = 24.dp),
+                        fontSize = 18.sp,
+                    )
+                    Button(
+                        onClick = {viewmodel.addEvent(
+                            Event(
+                                name = "Asado",
+                                date = "23/03",
+                                description = "Carne",
+                                totalAmount = 0.0
+                            )
+                        )},
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(horizontal = 16.dp),
+                        colors = ButtonDefaults.buttonColors(
+                            containerColor = Purple40,
+                            contentColor = Color.White
+                        ),
+                        elevation = ButtonDefaults.elevatedButtonElevation(8.dp)
+                    ) {
+                        Row(
+                            modifier = Modifier.padding(6.dp),
+                            horizontalArrangement = Arrangement.spacedBy(16.dp),
+                            verticalAlignment = Alignment.CenterVertically
+                        ) {
+                            Icon(
+                                Icons.Default.AddCircle,
+                                contentDescription = ""
+                            )
+                            Text(
+                                "Crear Nuevo Evento",
+                                fontSize = 20.sp,
+                                fontWeight = FontWeight.Bold
+                            )
+                        }
+                    }
+                }
+
+            }
+            LazyColumn() {
+                items(events){
+                    Text(it.name)
+                }
+            }
+        }
+
+    }
+}
