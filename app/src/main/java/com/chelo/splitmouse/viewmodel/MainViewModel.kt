17d0d3dd898b5416febce1a508203d16ff6de28f
Lautaro Ildarraz <112969613+Lautaro-io@users.noreply.github.com>
2026-04-10 17:25:34 -0300

    Se cambio el modo de recibir los eventos desde la base de datos usando Flow para mas reactividad y optimizacion

--- app/src/main/java/com/chelo/splitmouse/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/chelo/splitmouse/viewmodel/MainViewModel.kt
@@ -5,30 +5,28 @@ import androidx.lifecycle.viewModelScope
 import com.chelo.splitmouse.domain.model.Event
 import com.chelo.splitmouse.domain.repositories.EventRepository
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.WhileSubscribed
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
 
+@OptIn(ExperimentalStdlibApi::class)
 class MainViewModel(private val repo: EventRepository) : ViewModel() {
 
-    private val _uiState = MutableStateFlow<MainUiState>(MainUiState())
+    private val _uiState = repo.getAllEvents().map {
+        MainUiState(events = it)
+    }.catch {
+        emit(MainUiState(error = it.message, isLoading = false))
+    }.stateIn(
+        viewModelScope,
+        SharingStarted.WhileSubscribed(5000L),
+        initialValue = MainUiState(isLoading = true)
+    )
     val uiState = _uiState
 
 
-    init {
-        _uiState.value = _uiState.value.copy(isLoading = true)
-        viewModelScope.launch {
-            val events = repo.getAllEvents()
-            _uiState.value = _uiState.value.copy(isLoading = false , events = events)
-        }
-
-    }
-    fun addEvent(event: Event) {
-        _uiState.value = _uiState.value.copy(isLoading = true)
-        viewModelScope.launch {
-            repo.addEvent(event)
-            val events = repo.getAllEvents()
-            _uiState.value = _uiState.value.copy(isLoading = false , events = events)
-        }
-    }
 
 
 }
