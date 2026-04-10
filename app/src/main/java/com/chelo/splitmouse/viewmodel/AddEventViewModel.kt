bd1bebe4a8c207996ee7474c824f63c9cdbc4451
Lautaro Ildarraz <112969613+Lautaro-io@users.noreply.github.com>
2026-04-10 17:26:00 -0300

    Se creo un ViewModel especifico para el caso de uso de agregar un evento

---
+++ app/src/main/java/com/chelo/splitmouse/viewmodel/AddEventViewModel.kt
@@ -0,0 +1,69 @@
+package com.chelo.splitmouse.viewmodel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.chelo.splitmouse.domain.model.Event
+import com.chelo.splitmouse.domain.repositories.EventRepository
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+
+class AddEventViewModel(private val repo: EventRepository) : ViewModel() {
+
+    private val _formState = MutableStateFlow(EventFormState())
+    val formState = _formState.asStateFlow()
+
+
+    val isFormValid = _formState.map { state ->
+        state.name.isNotBlank() && state.date.isNotBlank()
+    }.stateIn(viewModelScope, SharingStarted.WhileSubscribed(4000L), false)
+
+
+    fun addEvent(onSuccess: () -> Unit) {
+        val event = _formState.value
+        viewModelScope.launch {
+            try {
+                repo.addEvent(
+                    Event(
+                        id = 0,
+                        name = event.name,
+                        description = event.description ?: "",
+                        date = event.date,
+                        totalAmount = 0.0
+                    )
+                )
+                _formState.update {
+                    EventFormState()
+                }
+                onSuccess()
+
+            }catch (e : Exception){}
+        }
+
+    }
+
+    fun updateFormState(field: FieldType, value: String) {
+        _formState.update {
+            when (field) {
+                FieldType.NAME -> it.copy(name = value)
+                FieldType.DESCRIPTION -> it.copy(description = value)
+                FieldType.DATE -> it.copy(date = value)
+            }
+        }
+    }
+
+
+}
+
+
+enum class FieldType { NAME, DESCRIPTION, DATE }
+
+data class EventFormState(
+    val name: String = "",
+    val description: String? = null,
+    val date: String = "",
+)
