0afbc77861ec9db2d973263815137623b20bfc65
Lautaro Ildarraz <112969613+Lautaro-io@users.noreply.github.com>
2026-04-17 20:09:11 -0300

    Se adapto para manejar el Flow

--- app/src/main/java/com/chelo/splitmouse/data/repositories/ParticipantRepositoryImpl.kt
+++ app/src/main/java/com/chelo/splitmouse/data/repositories/ParticipantRepositoryImpl.kt
@@ -5,8 +5,12 @@ import com.chelo.splitmouse.data.local.entities.toModel
 import com.chelo.splitmouse.domain.model.Participant
 import com.chelo.splitmouse.domain.model.toEntity
 import com.chelo.splitmouse.domain.repositories.ParticipantRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
 
-class ParticipantRepositoryImpl (val participantDao: ParticipantDao) : ParticipantRepository {
+class ParticipantRepositoryImpl(val participantDao: ParticipantDao) : ParticipantRepository {
+
+
     override suspend fun addParticipant(participant: Participant) {
         participantDao.insertParticipant(participant.toEntity())
     }
@@ -15,14 +19,11 @@ class ParticipantRepositoryImpl (val participantDao: ParticipantDao) : Participa
         TODO("Not yet implemented")
     }
 
-    override suspend fun getParticipantsByEvent(eventId: Long): List<Participant> {
-        return participantDao.getParticipantsByEvent(eventId).map {
-            it.toModel()
+    override fun getParticipantsByEvent(eventId: Long): Flow<List<Participant>> {
+        return participantDao.getParticipantsByEvent(eventId).map { list ->
+            list.map {
+                it.toModel()
+            }
         }
-
     }
-
-
-
-
 }
