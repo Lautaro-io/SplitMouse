abad2967c0108bf9dd2080e45ca18594c6cf006d
Lautaro Ildarraz <112969613+Lautaro-io@users.noreply.github.com>
2026-04-16 22:17:19 -0300

    se modifico el composable de entrada para hacer uso de Navigation

--- app/src/main/java/com/chelo/splitmouse/MainActivity.kt
+++ app/src/main/java/com/chelo/splitmouse/MainActivity.kt
@@ -4,14 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import com.chelo.splitmouse.screens.MainScreen
+import com.chelo.splitmouse.ui.navigation.AppNavigation
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            MainScreen()
+            AppNavigation()
         }
 
     }
