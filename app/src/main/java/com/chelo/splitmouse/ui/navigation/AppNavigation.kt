2c137ca4311e874119370545e507e7bd53240872
Lautaro Ildarraz <112969613+Lautaro-io@users.noreply.github.com>
2026-04-16 22:14:34 -0300

    Se implemento Navigation para la navegacion entre pantallas

---
+++ app/src/main/java/com/chelo/splitmouse/ui/navigation/AppNavigation.kt
@@ -0,0 +1,42 @@
+package com.chelo.splitmouse.ui.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.toRoute
+import com.chelo.splitmouse.ui.screens.EventDetailScreen
+import com.chelo.splitmouse.ui.screens.MainScreen
+import com.chelo.splitmouse.viewmodel.EventDetailViewModel
+import org.koin.androidx.compose.koinViewModel
+import org.koin.core.parameter.parametersOf
+
+@Composable
+fun AppNavigation(navController: NavHostController = rememberNavController()) {
+
+    NavHost(navController, startDestination = Home) {
+
+
+        composable<Home> {
+            MainScreen(navigateToDetail = {
+                navController.navigate(EventDetail(it))
+            })
+        }
+
+        composable<EventDetail> { backStackEntry ->
+            val eventDetails: EventDetail = backStackEntry.toRoute()
+            val vm: EventDetailViewModel = koinViewModel { parametersOf(eventDetails.eventId) }
+            EventDetailScreen(
+                onBack = {
+                    navController.navigate(Home) {
+                        popUpTo(Home) {
+                            inclusive = true
+                        }
+                    }
+                },
+                viewModel = vm
+            )
+        }
+    }
+}
