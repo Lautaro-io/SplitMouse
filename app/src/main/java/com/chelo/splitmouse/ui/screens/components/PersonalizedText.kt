b87c8ab108169c5ad46701fc2ea686a93ad6777f
Lautaro Ildarraz <112969613+Lautaro-io@users.noreply.github.com>
2026-04-20 21:06:31 -0300

    Se creo PersonalizedText.kt para dar enfasis a ciertos textos en la app

---
+++ app/src/main/java/com/chelo/splitmouse/ui/screens/components/PersonalizedText.kt
@@ -0,0 +1,29 @@
+package com.chelo.splitmouse.ui.screens.components
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.unit.sp
+import com.chelo.splitmouse.ui.theme.VioletaFuerte
+
+@Composable
+fun PersonalizedText( fullText : String , highlightText : List<String> , fontSize : Int ){
+    val annotatedString = buildAnnotatedString {
+        val parts = fullText.split(" ")
+        parts.forEach { word ->
+            if (highlightText.contains(word)){
+                withStyle(style = SpanStyle(color = VioletaFuerte , fontWeight = FontWeight.Bold)){
+                    append("$word " )
+                }
+            }else{
+                append("$word ")
+            }
+
+        }
+    }
+    Text(text = annotatedString, fontSize = fontSize.sp, fontWeight = FontWeight.Bold)
+
+}
