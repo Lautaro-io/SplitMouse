22cb1281d898a32f8e00f1eb648380827d687c01
Lautaro Ildarraz <112969613+Lautaro-io@users.noreply.github.com>
2026-04-20 21:10:50 -0300

    se agrego metodo para actualizar el evento

--- app/src/main/java/com/chelo/splitmouse/data/local/daos/EventDao.kt
+++ app/src/main/java/com/chelo/splitmouse/data/local/daos/EventDao.kt
@@ -4,6 +4,7 @@ import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
+import androidx.room.Update
 import com.chelo.splitmouse.data.local.entities.EventEntity
 import kotlinx.coroutines.flow.Flow
 
@@ -13,6 +14,10 @@ interface EventDao {
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertEvent(event: EventEntity)
+
+
+    @Update
+    suspend fun updateEvent(event: EventEntity)
 
     @Query("SELECT * FROM events")
     fun getEvents(): Flow<List<EventEntity>>
