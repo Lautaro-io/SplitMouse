35cd1382736a8e3fb8c0ea14d925fb99c18a082f
Lautaro Ildarraz <112969613+Lautaro-io@users.noreply.github.com>
2026-04-20 21:10:34 -0300

    se agregraron variables para poder agregrar el gasto a la db

--- app/src/main/java/com/chelo/splitmouse/ui/screens/EventDetailScreen.kt
+++ app/src/main/java/com/chelo/splitmouse/ui/screens/EventDetailScreen.kt
@@ -1,6 +1,7 @@
 package com.chelo.splitmouse.ui.screens
 
 import androidx.activity.compose.BackHandler
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -13,13 +14,15 @@ import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AddCard
-import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.ArrowBackIosNew
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
@@ -28,15 +31,16 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.chelo.splitmouse.domain.model.Participant
 import com.chelo.splitmouse.ui.screens.components.AddExpenseBottomSheet
 import com.chelo.splitmouse.ui.screens.components.AddParticipantDialog
 import com.chelo.splitmouse.ui.screens.components.ContentDetailHeader
+import com.chelo.splitmouse.ui.theme.Pink40
 import com.chelo.splitmouse.ui.theme.VioletaFuerte
 import com.chelo.splitmouse.viewmodel.EventDetailViewModel
 
@@ -47,7 +51,12 @@ fun EventDetailScreen(onBack: () -> Unit, viewModel: EventDetailViewModel) {
     val state by viewModel.uiState.collectAsState()
     var showDialogParticipant by remember { mutableStateOf(false) }
     var showBottomModal by remember { mutableStateOf(false) }
-
+    var showDeleteParticipantDialog by remember { mutableStateOf(false) }
+    var participantId: Long? by remember { mutableStateOf(null) }
+    val bgColor = Brush.verticalGradient(
+        0.7f to Color.Transparent,
+        1.0f to Pink40
+    )
     BackHandler {
         onBack()
     }
@@ -63,7 +72,7 @@ fun EventDetailScreen(onBack: () -> Unit, viewModel: EventDetailViewModel) {
             ) {
                 IconButton(onClick = onBack) {
                     Icon(
-                        imageVector = Icons.Default.ArrowBack,
+                        imageVector = Icons.Default.ArrowBackIosNew,
                         contentDescription = "",
                         modifier = Modifier.size(32.dp),
                         tint = VioletaFuerte
@@ -102,27 +111,53 @@ fun EventDetailScreen(onBack: () -> Unit, viewModel: EventDetailViewModel) {
             }
         }
     ) { innerPadding ->
-        if (showDialogParticipant) {
-            AddParticipantDialog(
-                onDismiss = { showDialogParticipant = false },
-                onConfirm = {
-                    viewModel.addParticipant(Participant(0, it, state.event?.id!!))
-                    showDialogParticipant = false
-                })
+        when {
+            showDialogParticipant -> {
+                AddParticipantDialog(
+                    onDismiss = { showDialogParticipant = false },
+                    onConfirm = {
+                        viewModel.addParticipant(it)
+                        showDialogParticipant = false
+                    })
 
+            }
+
+            showDeleteParticipantDialog -> {
+                participantId?.let {
+                    DeleteParticipantDialog(
+                        onDismiss = { showDeleteParticipantDialog = false },
+                        onConfirm = {
+                            viewModel.deleteParticipant(it)
+                            showDeleteParticipantDialog = false
+                        },
+                        name = (state.participants.find { participant -> participant.id == participantId }?.name
+                            ?: "")
+                    )
+                }
+
+            }
         }
+
         Column(
             modifier = Modifier
                 .fillMaxSize()
+                .background(bgColor)
                 .padding(innerPadding),
             horizontalAlignment = Alignment.CenterHorizontally,
         ) {
 
             ContentDetailHeader(
                 detailViewModel = viewModel,
-                onAddParticipantClick = { showDialogParticipant = true })
+                onAddParticipantClick = { showDialogParticipant = true },
+                onDeleteParticipant = { showDeleteParticipantDialog = true; participantId = it })
             if (showBottomModal) {
-                AddExpenseBottomSheet(onDismiss = { showBottomModal = false }, state.participants)
+                AddExpenseBottomSheet(
+                    onDismiss = { showBottomModal = false },
+                    state.participants,
+                    onAddExpenseClick = {
+                        amount, description, payerId -> viewModel.addExpense(amount, description, payerId)
+                        showBottomModal = false
+                    })
             }
 
         }
@@ -130,7 +165,27 @@ fun EventDetailScreen(onBack: () -> Unit, viewModel: EventDetailViewModel) {
     }
 }
 
-
+@Composable
+fun DeleteParticipantDialog(onDismiss: () -> Unit, onConfirm: () -> Unit, name: String) {
+    AlertDialog(
+        containerColor = Color.White,
+        onDismissRequest = onDismiss,
+        title = { Text("Eliminar Participante", fontWeight = FontWeight.Bold) },
+        text = {
+            Text("Estas seguro que quieres eliminar a $name")
+        },
+        confirmButton = {
+            TextButton(onClick = { onConfirm() }) {
+                Text("Eliminar", color = Color.Red)
+            }
+        },
+        dismissButton = {
+            TextButton(onClick = { onDismiss() }) {
+                Text("Cancelar")
+            }
+        }
+    )
+}
 
 
 
