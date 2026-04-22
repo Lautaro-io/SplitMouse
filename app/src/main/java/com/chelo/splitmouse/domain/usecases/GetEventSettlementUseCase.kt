0ee6a084650e33648243e657ab460ec3fc1409c2
Lautaro Ildarraz <112969613+Lautaro-io@users.noreply.github.com>
2026-04-22 20:20:37 -0300

    Se modifico la logica de repartija ya que tenia un bug que daba al reves los debts

--- app/src/main/java/com/chelo/splitmouse/domain/usecases/GetEventSettlementUseCase.kt
+++ app/src/main/java/com/chelo/splitmouse/domain/usecases/GetEventSettlementUseCase.kt
@@ -1,9 +1,11 @@
 package com.chelo.splitmouse.domain.usecases
 
+import android.util.Log
 import com.chelo.splitmouse.domain.model.Balance
 import com.chelo.splitmouse.domain.model.Debt
 import com.chelo.splitmouse.domain.model.Expense
 import com.chelo.splitmouse.domain.model.Participant
+import kotlin.math.abs
 
 class GetEventSettlementUseCase {
 
@@ -24,10 +26,20 @@ class GetEventSettlementUseCase {
 
         while (debts.isNotEmpty() && creditors.isNotEmpty()) {
             val debtor = debts[0]
+            Log.d("CHELO", debtor.participantName)
             val creditor = creditors[0]
+            Log.d("CHELO", creditor.participantName)
 
 
-            val amountToPay = minOf(-debtor.balance, creditor.balance)
+            val debtAmount = -(debtor.balance)
+            Log.d("CHELO", debtAmount.toString())
+
+            val creditorBalance = creditor.balance
+            Log.d("CHELO", creditorBalance.toString())
+
+            val amountToPay = minOf(debtAmount, creditorBalance)
+            Log.d("CHELO", amountToPay.toString())
+
             listDebts.add(
                 Debt(
                     from = debtor.participantName,
@@ -35,11 +47,12 @@ class GetEventSettlementUseCase {
                     amount = amountToPay
                 )
             )
+
             debtor.balance += amountToPay
             creditor.balance -= amountToPay
 
-            if (debtor.balance < 0.1) debts.removeAt(0)
-            if (creditor.balance < 0.1) creditors.removeAt(0)
+            if (abs(debtor.balance) < 0.1) debts.removeAt(0)
+            if (abs(creditor.balance) < 0.1) creditors.removeAt(0)
         }
 
 
