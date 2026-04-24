ef3c3fa2a47c63336c48a2c4392ee0990cf46aea
Lautaro Ildarraz <112969613+Lautaro-io@users.noreply.github.com>
2026-04-23 21:36:19 -0300

    Se agrego la pantalla DebtDetail

--- app/src/main/java/com/chelo/splitmouse/ui/navigation/AppNavigation.kt
+++ app/src/main/java/com/chelo/splitmouse/ui/navigation/AppNavigation.kt
@@ -8,6 +8,7 @@ import androidx.navigation.compose.rememberNavController
 import androidx.navigation.toRoute
 import com.chelo.splitmouse.ui.screens.EventDetailScreen
 import com.chelo.splitmouse.ui.screens.MainScreen
+import com.chelo.splitmouse.ui.screens.components.SettlementScreen
 import com.chelo.splitmouse.viewmodel.EventDetailViewModel
 import org.koin.androidx.compose.koinViewModel
 import org.koin.core.parameter.parametersOf
@@ -35,7 +36,31 @@ fun AppNavigation(navController: NavHostController = rememberNavController()) {
                         }
                     }
                 },
+                navToDebt = {eventId ->
+                    navController.navigate(DebtDetail(eventId)){
+                        popUpTo(Home){
+                            inclusive = true
+                        }
+                    }
+                },
                 viewModel = vm
+            )
+        }
+
+
+        composable<DebtDetail> { backStackEntry ->
+            val eventDetails: EventDetail = backStackEntry.toRoute()
+            val vm: EventDetailViewModel = koinViewModel { parametersOf(eventDetails.eventId) }
+            SettlementScreen(
+                onBack = {
+                    navController.navigate(Home) {
+                        popUpTo(Home) {
+                            inclusive = true
+                        }
+                    }
+                },
+                vm
+
             )
         }
     }
