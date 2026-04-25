aab1dd3093d81c19bffe93411b79eed20f32c3c8
Lautaro Ildarraz <112969613+Lautaro-io@users.noreply.github.com>
2026-04-25 20:08:45 -0300

    Se agrego metodo para formatear la fecha

--- app/src/main/java/com/chelo/splitmouse/ui/NumberFormatter.kt
+++ app/src/main/java/com/chelo/splitmouse/ui/NumberFormatter.kt
@@ -1,6 +1,8 @@
 package com.chelo.splitmouse.ui
 
 import java.text.NumberFormat
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
 import java.util.Locale
 
 
@@ -12,4 +14,14 @@ val currencyFormatter: NumberFormat = NumberFormat.getCurrencyInstance(argentina
 
 fun Double.toArgentineCurrency(): String {
     return currencyFormatter.format(this)
+}
+
+fun formatFecha(fechaInput: String): String {
+    val formatterEntrada = DateTimeFormatter.ofPattern("dd/MM/yyyy")
+
+    val fecha = LocalDate.parse(fechaInput, formatterEntrada)
+
+    val formatterSalida = DateTimeFormatter.ofPattern("EEEE, d 'de' MMMM", Locale("es", "ES"))
+
+    return fecha.format(formatterSalida).replaceFirstChar { it.uppercase() }
 }
