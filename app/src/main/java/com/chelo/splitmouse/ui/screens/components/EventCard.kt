a308517e00bd07ebf86e16ba9fd13aa0ac7f24b0
Lautaro Ildarraz <112969613+Lautaro-io@users.noreply.github.com>
2026-04-24 21:33:16 -0300

    Se modifico la forma por un rectangleshape

--- app/src/main/java/com/chelo/splitmouse/ui/screens/components/EventCard.kt
+++ app/src/main/java/com/chelo/splitmouse/ui/screens/components/EventCard.kt
@@ -6,12 +6,14 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
@@ -27,6 +29,7 @@ fun EventCard(event: Event, onEventClick: () -> Unit) {
             .fillMaxWidth()
             .padding(8.dp)
             .clickable { onEventClick() },
+        shape = RectangleShape,
         elevation = CardDefaults.cardElevation(4.dp),
         colors = CardDefaults.cardColors(containerColor = Color.White)
     ) {
